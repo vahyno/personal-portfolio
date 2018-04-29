ca9204238315b6d83e43cd4bf43a3d679715d157
vahyno <vahyno@gmail.com>
2018-04-29 09:55:20 -0700

    updated favicon

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,8 @@ console.log("app.js linked.");
 $(document).ready(function () {
   console.log('my document has been loaded!');
 
+  // $('#body').append(``);
 
+  // console.log('appended')
 
   });
